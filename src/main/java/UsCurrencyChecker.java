6bcf63e2e2b92a97de136f5a9610c5071344ad3b
Daniel <daniel.kastenhofer1@gmail.com>
2020-05-25 08:57:04 +0200

    Final Commit

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,16 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        final String regex = "\\$\\s?\\d+(\\.?\\d{1,2})?";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        Matcher matcher = pattern.matcher(usCurrencyString);
+
+        return matcher;
     }
 }
